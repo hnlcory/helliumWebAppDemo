fab505fbf98b7755eb529986819a19805d3f969f
Cory-Mac <school.coryparker@gmail.com>
2022-03-31 16:47:41 -1000

    button fixes on homepage

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Grid, Image } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
@@ -11,8 +12,12 @@ class Landing extends React.Component {
             <Image size='small' src='/images/symbol.jpeg' centered></Image>
             <h1 id='font-type-main'>hntHI</h1>
             <p id='font-type-general'>Data Visualization for Helium LoRaWAN Miners</p>
-            <button className="ui button black">Login</button>
-            <button className="ui button black">Signup</button>
+            <Link to='/signin'>
+              <button className="ui button black" >Login</button>
+            </Link>
+            <Link to='/signup'>
+              <button className="ui button black" >Signup</button>
+            </Link>
           </Grid.Column>
           <Grid.Column>
             <Image size='huge' src='/images/longfilarge.png' />
